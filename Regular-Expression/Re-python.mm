6126aedf9f1f22c8dab7444eeb29ebe51a4dbdd6
jedsada-io <jedsada.io@outlook.com>
2021-02-15 20:32:54 +0700

    Add mindmap and Regex-pdf-note

---
+++ Regular-Expression/Re-python.mm
@@ -0,0 +1,4 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1613395535452" ID="ID_1782130115" MODIFIED="1613395535452" TEXT="New Mindmap"/>
+</map>
