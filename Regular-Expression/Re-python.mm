15401db3820b03d2bc12a53498d1bc5821b6b99f
jedsada-io <jedsada.io@outlook.com>
2021-02-16 11:44:30 +0700

    Modify

--- Regular-Expression/Re-python.mm
+++ Regular-Expression/Re-python.mm
@@ -1,4 +1,13 @@
 <map version="1.1.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1613395535452" ID="ID_1782130115" MODIFIED="1613395535452" TEXT="New Mindmap"/>
+<node CREATED="1613395535452" ID="ID_1782130115" MODIFIED="1613395570903" TEXT="Regular expression python">
+<node CREATED="1613395571675" ID="ID_1267611464" MODIFIED="1613395595150" POSITION="right" TEXT="Regex process ">
+<node CREATED="1613395598538" ID="ID_1449400038" MODIFIED="1613395609651" TEXT="1,import re"/>
+<node CREATED="1613395610867" ID="ID_1237419501" MODIFIED="1613395616216" TEXT="2.Create regex object "/>
+<node CREATED="1613395617008" ID="ID_1051299877" MODIFIED="1613395629530" TEXT="3.Use regex method to search/sub"/>
+</node>
+<node CREATED="1613395632400" ID="ID_1845797422" MODIFIED="1613395643189" POSITION="left" TEXT="Regex optional symbol"/>
+<node CREATED="1613395643880" ID="ID_1277042319" MODIFIED="1613395650218" POSITION="left" TEXT="Regex method "/>
+<node CREATED="1613395653550" ID="ID_1412042520" MODIFIED="1613395666413" POSITION="left" TEXT="Second argument of re.compile() "/>
+</node>
 </map>
